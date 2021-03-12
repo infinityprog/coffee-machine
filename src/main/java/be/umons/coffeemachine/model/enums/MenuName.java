1a6becb467574229dd424a2a197e6b1cb843047c
hugocianciolo <hugo220798@gmail.com>
2021-03-12 21:15:58 +0100

    Menu enum

---
+++ src/main/java/be/umons/coffeemachine/model/enums/MenuName.java
@@ -0,0 +1,43 @@
+package be.umons.coffeemachine.model.enums;
+
+public enum MenuName {
+    Favori("Favoris"),
+
+    SETTINGS_DRINK_QUANTITY("Réglage de la quantité"),
+
+    WATER_HARDNESS("Dureté de l'eau"),
+
+    STOP_AUTO("Arrêt automatique"),
+
+    COFFEE_TEMPERATURE("Température du caffé"),
+
+    WATER_FILTER("Filtre à eau"),
+
+    FROST_PROTECTION("Protection contre le gel"),
+
+    BIP_SONORE("Bip sonore"),
+
+    DRINK_SERVED("Boissons servies"),
+
+    RESET("Préréglage d'usine"),
+
+    CALC_AND_CLEAN("Calc'nClean"),
+
+    CLEANING_MILK_FROTH("Nettoyage du mousseur de laît"),
+
+    CLEANING("Nettoyage"),
+
+    DESCALING("Détartrage")
+
+    ;
+
+    private String name;
+
+    MenuName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/be/umons/coffeemachine/model/factory/MenuFactory.java
@@ -0,0 +1,16 @@
+package be.umons.coffeemachine.model.factory;
+
+import be.umons.coffeemachine.model.enums.MenuName;
+import be.umons.coffeemachine.state.menu.Menu;
+
+public class MenuFactory {
+
+    public Menu getMenu(MenuName name) {
+
+        switch (name) {
+
+        }
+
+        return null;
+    }
+}
